a028ddc204fde24cde1d53ee39b08f7001e6933b
mohit <agarwal.mohit421@gmail.com>
2021-03-23 01:46:52 +0530

    added swagger

--- server.js
+++ server.js
@@ -2,41 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-//const basicAuth = require("express-basic-auth");
 var http = require("http").createServer(app);
-require("dotenv").config(".env");
-
+require("dotenv").config({ path: __dirname + "/../.env" });
+const swaggerUi  = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 var corsOptions = { origin: true, credentials: true };
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, (...args) => swaggerUi.setup(swaggerDocument)(...args));
 const db = require("./app/models");
 db.sequelize.sync();
 
-/*
-app.use(
-  basicAuth({
-    authorizer: (username, password, cb) => {
-      const userMatches = basicAuth.safeCompare(
-        username,
-        process.env.AUTH_USERNAME
-      );
-      const passwordMatches = basicAuth.safeCompare(
-        password,
-        process.env.AUTH_PASSWORD
-      );
-      if (userMatches & passwordMatches) return cb(null, true);
-      else return cb(null, false);
-    },
-    authorizeAsync: true,
-    unauthorizedResponse: (req) => {
-      return `unauthorized. ip: ${req.ip}`;
-    },
-    challenge: true,
-  })
-);
-*/
 require("./app/routes/stock.routes")(app);
 
 const PORT = process.env.SERVER_PORT || 8000;
